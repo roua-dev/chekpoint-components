96078e049e5ad77f9786b048f572d5c6f1a714e1
roua-dev <ghanmiroua7@gmail.com>
2021-07-08 09:13:24 +0100

    firstcommit

---
+++ src/components/MainVideo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainVideo = () => {
+    return (
+        <div>
+            <iframe width="100%" height="500" src="https://www.youtube.com/embed/Y2hgEGPzTZY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+    )
+}
+
+export default MainVideo
